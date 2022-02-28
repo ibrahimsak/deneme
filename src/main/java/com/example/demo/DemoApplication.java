0599a027711e6933d406346cc7a414ae65c967e2
Kemal Yuksel <kemalyksell98@gmail.com>
2022-02-28 21:00:57 +0300

    commit :)

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,9 @@ public class DemoApplication
     public static void main(String[] args)
     {
         SpringApplication.run(DemoApplication.class, args);
+
+        System.out.println(testClass.sum(10,5));
+
     }
 
 }
---
+++ src/main/java/com/example/demo/testClass.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class testClass {
+
+    static int sum(int x,int y){
+        return x+y;
+    }
+
+
+}
